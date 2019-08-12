6e0c027ea74e3dd2bca11971c92ad53600f884ed
volvo007 <foxprowing@163.com>
2019-08-12 12:47:56 +0800

    f(Reset) - reset elements on the page

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 /*
  * 创建一个包含所有卡片的数组
  */
-
+let deck = document.querySelector('.deck')
+let cards = document.querySelectorAll('.card')
+    // notice that cards is not an array, but a NodeArray
 
 /*
  * 显示页面上的卡片
@@ -12,7 +14,8 @@
 
 // 洗牌函数来自于 http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -25,6 +28,47 @@ function shuffle(array) {
     return array;
 }
 
+function resetCards() {
+    // temp_cards = cards;
+    tempCards = Array.prototype.slice.call(cards);
+    tempCards = shuffle(tempCards);
+    for (let i = 0; i < tempCards.length; i++) {
+        tempCards[i].className = 'card';
+        deck.appendChild(tempCards[i]);
+    }
+}
+
+// listener of button "restrat"
+function clickRestart() {
+    document.querySelector('.restart').addEventListener('click', restart);
+}
+
+let count = 0; // clicks count
+let openList = []; // already opened cards
+let matchList = []; // only allow two cards in the list maximum
+let timeStart = timeEnd = 0;
+let timer;
+
+function restart() {
+    resetCards();
+    count = 0;
+    document.querySelector('.moves').textContent = '0';
+    openList = [];
+    matchList = [];
+    resetStar();
+}
+
+// reset stars to black ones
+function resetStar() {
+    let stars = document.querySelector('.stars').children;
+    for (let item of stars) {
+        item.innerHTML = '<i class="fa fa-star"></i>';
+    }
+}
+
+// cards.forEach(function(item) {
+//     item.className = 'card open show';
+// });
 
 /*
  * 设置一张卡片的事件监听器。 如果该卡片被点击：
@@ -36,3 +80,10 @@ function shuffle(array) {
  *    + 增加移动计数器并将其显示在页面上（将这个功能放在你从这个函数中调用的另一个函数中）
  *    + 如果所有卡都匹配，则显示带有最终分数的消息（将这个功能放在你从这个函数中调用的另一个函数中）
  */
+
+
+window.onload = function() {
+    resetCards();
+    document.querySelector('.moves').textContent = '0';
+    clickRestart();
+}
